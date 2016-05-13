bcced98229eec08cbea626397c19ccb1b9c22b0c
300872772 <mamun_131@hotmail.com>
2016-05-13 10:51:44 -0400

    Hab entered comments

--- COMP123-S2016-Lession1B/Program.cs
+++ COMP123-S2016-Lession1B/Program.cs
@@ -4,6 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ *Author: Md Mamunur Rahman
+ * Date: May 13, 2016
+ * Student ID: 300872772 
+ * 
+ * Description: This program demostrait GitHub and Version control
+ * best practice. We also explore Unit Testing
+ * 
+ * Version: 0.1 - Initial Version
+ */
+
+
+
+
 namespace COMP123_S2016_Lession1B
 {
     class Program
